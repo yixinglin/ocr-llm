bf81f5d1c7265fe6499b62278626b2368bd2e9ca
Yixing Lin <184059914@qq.com>
2024-09-17 01:08:13 +0200

    Update Gradio for ocrllm

--- backend/gui.py
+++ backend/gui.py
@@ -3,7 +3,17 @@ from io import BytesIO
 import pandas as pd
 import gradio as gr
 import requests
+import json
 
+def load_config():
+    with open('conf/gui.json', 'r') as f:
+        config = json.load(f)
+    return config
+
+config = load_config()
+username = config["ocrllm-api"]["username"]
+password = config["ocrllm-api"]["password"]
+base_url = config["ocrllm-api"]["url"]
 
 def recognize_text(image):
     # 使用 Tesseract 从图片中提取文本
@@ -18,8 +28,8 @@ def recognize_text(image):
         "im_preprocess": True,
         "enable_bbox": False,
     }
-    url = "http://localhost:6995/v1/ocrllm/extract-quote-image/base64/"
-    response = requests.post(url, json=body).json()
+    url = f"{base_url}/v1/ocrllm/extract-quote-image/base64/"
+    response = requests.post(url, json=body, auth=(username, password)).json()
     answer = response["data"]["llm"]["answer"]
     orderlines = answer["orderlines"]
     df = pd.DataFrame(orderlines)
@@ -33,14 +43,7 @@ iface = gr.Interface(
     outputs=gr.Dataframe(label="Extracted Text")
 )
 
-# with gr.Blocks() as iface:
-#     gr.Markdown("## Text Recognition from Image")
-#     with gr.Row():
-#         image = gr.Image(type='pil', label="Upload Image")
-#     with gr.Row():
-#         output = gr.Dataframe(label="Extracted Text")
-#     image.change(recognize_text, inputs=[image], outputs=[output])
 
 # 启动应用
 if __name__ == "__main__":
-    iface.launch(server_name="0.0.0.0", server_port=6998)
+    iface.launch(server_name="0.0.0.0", server_port=6998, root_path="/ocrllm-gui")
